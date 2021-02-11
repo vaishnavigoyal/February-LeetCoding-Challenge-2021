d655626843b90a45ac4c3435116b2d6c141f9d8a
vaishnavigoyal4 <vaishnavigoyal4@gmail.com>
2021-02-11 19:54:21 +0530

    Day 11 : Valid Anagram

---
+++ Day 11.cpp
@@ -0,0 +1,14 @@
+// Day 11
+// Question : Valid Anagram
+
+class Solution
+{
+public:
+	bool isAnagram(string s, string t)
+	{
+		sort(s.begin(), s.end());
+		sort(t.begin(), t.end());
+
+		return (s == t);
+	}
+};
