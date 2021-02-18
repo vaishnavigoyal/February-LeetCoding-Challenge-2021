6982908e99dc3e7011608b0e73b281ffe072fce0
vaishnavigoyal4 <vaishnavigoyal4@gmail.com>
2021-02-18 15:15:59 +0530

    Day 16 : Letter Case Permutation

---
+++ Day 16.cpp
@@ -0,0 +1,34 @@
+// Day 16
+// Question : Letter Case Permutation
+
+class Solution
+{
+public:
+	void solve(string S, vector<string> &ans, int i)
+	{
+		if (i == S.size())
+		{
+			ans.push_back(S);
+			return;
+		}
+
+		if (isalpha(S[i]))
+		{
+			S[i] = toupper(S[i]);
+			solve(S, ans, i + 1);
+			S[i] = tolower(S[i]);
+			solve(S, ans, i + 1);
+		}
+		else
+		{
+
+			solve(S, ans, i + 1);
+		}
+	}
+	vector<string> letterCasePermutation(string S)
+	{
+		vector<string> ans;
+		solve(S, ans, 0);
+		return ans;
+	}
+};
